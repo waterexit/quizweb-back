36b3f7697b1402af891674302d2eb6304e17e09c
root <waterexot1120@gmai.com>
2021-04-04 11:03:55 +0000

    eject + test

---
+++ src/main/java/quizweb/app/restcontroller/response/Question.java
@@ -0,0 +1,16 @@
+package quizweb.app.restcontroller.response;
+
+public class Question {
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+
+
+}
