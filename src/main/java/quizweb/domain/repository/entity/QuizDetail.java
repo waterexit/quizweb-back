12fa6991c651f3dcd54a5375e7656f4fc19d7cb0
water <waterexit1120@gmail.com>
2023-01-22 11:33:38 +0000

    squash

--- src/main/java/quizweb/domain/repository/entity/QuizDetail.java
+++ src/main/java/quizweb/domain/repository/entity/QuizDetail.java
@@ -6,8 +6,14 @@ public class QuizDetail extends Quiz {
 
     private List<QuestionDetail> questions;
 
+    private List<Tag> tags;
+
     public List<QuestionDetail> getQuestions() {
         return questions;
+    }
+
+    public List<Tag> geTags() {
+        return tags;
     }
 
 }
