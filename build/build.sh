bfecee04752c06b717bd55ed8e18af7b67907442
water <waterexit1120@gmail.com>
2023-04-15 12:57:05 +0000

    クラス削除の際にゴミが残るのでデプロイの前に過去のwarそのものを消すように修正

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+mvn clean
 mvn package -DskipTests=true
 # mvn package spring-boot:repackage -DskipTests=true
+rm -rf /workspaces/quizweb/build/deploy/quizWeb.war
 mkdir -p /workspaces/quizweb/build/deploy
 cp -Pf /workspaces/quizweb/target/quizWeb.war /workspaces/quizweb/build/deploy/quizWeb.war
